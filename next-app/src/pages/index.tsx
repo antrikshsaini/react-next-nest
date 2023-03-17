4c726411c02af93a1cd7ba32768a4988fc22bf44
Antriksh Saini <sainiantriksh6@gmail.com>
2023-03-16 22:52:36 -0600

    test ssh key working

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline">Hello Test</h1>
       </main>
     </>
   );
